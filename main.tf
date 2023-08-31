9512ebb7b3bf1b4a907734e00433bef66ddc171e
Hariprasad29kc <137869631+Hariprasad29kc@users.noreply.github.com>
2023-08-31 14:21:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
     region = "us-east-1"  
 }
 
-resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+resource "aws_instance" "kcc" {
+  ami           = "ami-053b0d53c279acc90" # us-west-2
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
